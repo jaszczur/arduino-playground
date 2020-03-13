e4ebfb5b35496fac825aac91ed5a5a132fac39fc
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2020-03-13 22:02:29 +0100

    First project ;)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,31 @@
 #include <Arduino.h>
+const uint8_t LED_EXT = 12;
+const uint16_t baseTime = 1000;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_EXT, OUTPUT);
+}
+
+void basic() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(baseTime);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void fancy() {
+  for (uint16_t i = 0; i < 20; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(i * baseTime/100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(i * baseTime/200);
+  }
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+  digitalWrite(LED_EXT, HIGH);
+  basic();
+  // fancy();
+  digitalWrite(LED_EXT, LOW);
+  delay(baseTime);
 }
