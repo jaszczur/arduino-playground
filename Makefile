89c20eb4e29c8b55dd3e756f43658b986209650b
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2020-03-15 18:44:48 +0100

    Traffic lights

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@
 compile:
 	platformio run
 
+check:
+	platformio check
+
 upload:
 	platformio run -t upload
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,58 @@
 #include <Arduino.h>
-const uint8_t LED_EXT = 12;
-const uint16_t baseTime = 1000;
+const uint8_t LED_RED = 8;
+const uint8_t LED_YELLOW = 9;
+const uint8_t LED_GREEN= 10;
+const uint8_t LED_BLUE = 11;
+const uint8_t SWITCH = 2;
+
+const uint16_t sleepTime = 50;
+const uint16_t userTime = 300;
+
+void blink(uint8_t led) {
+  digitalWrite(led, HIGH);
+  delay(sleepTime);
+  digitalWrite(led, LOW);
+}
+
+void turnOnIf(uint8_t led, uint8_t onlyOnLed) {
+  digitalWrite(led, led == onlyOnLed ? HIGH : LOW);
+}
+
+void turnOnOnly(uint8_t led) {
+  turnOnIf(LED_RED, led);
+  turnOnIf(LED_YELLOW, led);
+  turnOnIf(LED_GREEN, led);
+  turnOnIf(LED_BLUE, led);
+}
+
+void sayHi() {
+  blink(LED_BLUE);
+  delay(userTime);
+}
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED_EXT, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(SWITCH, INPUT_PULLUP);
+
+  turnOnOnly(0); // turn off all
 }
 
-void basic() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(baseTime);
-  digitalWrite(LED_BUILTIN, LOW);
-}
-
-void fancy() {
-  for (uint16_t i = 0; i < 20; i++) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(i * baseTime/100);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(i * baseTime/200);
-  }
+void waitForSwitch() {
+  delay(userTime);
+  while(digitalRead(SWITCH) == HIGH) {}
 }
 
 void loop() {
-  digitalWrite(LED_EXT, HIGH);
-  basic();
-  // fancy();
-  digitalWrite(LED_EXT, LOW);
-  delay(baseTime);
+  turnOnOnly(LED_RED);
+  sayHi();
+  waitForSwitch();
+  turnOnOnly(LED_YELLOW);
+  delay(userTime * 3);
+  turnOnOnly(LED_GREEN);
+  delay(userTime * 8);
+  turnOnOnly(LED_YELLOW);
+  delay(userTime * 3);
 }
