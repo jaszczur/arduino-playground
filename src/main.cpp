94a58bc8e9a658eed16569a3de6a1ffa22933765
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2020-03-19 18:09:25 +0100

    Light direction probe

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
-const uint8_t LED_RED = 8;
-const uint8_t LED_YELLOW = 9;
-const uint8_t LED_GREEN= 10;
-const uint8_t LED_BLUE = 11;
-const uint8_t SWITCH = 2;
+const uint8_t LED_LL = 6;
+const uint8_t LED_L = 7;
+const uint8_t LED_C= 8;
+const uint8_t LED_R = 9;
+const uint8_t LED_RR = 10;
 
 const uint16_t sleepTime = 50;
 const uint16_t userTime = 300;
+const byte calibrationTimes = 20;
+
+int left = 0;
+int right = 0;
+int diff = 0;
+int calibration = 0;
 
 void blink(uint8_t led) {
   digitalWrite(led, HIGH);
@@ -19,40 +25,69 @@ void turnOnIf(uint8_t led, uint8_t onlyOnLed) {
 }
 
 void turnOnOnly(uint8_t led) {
-  turnOnIf(LED_RED, led);
-  turnOnIf(LED_YELLOW, led);
-  turnOnIf(LED_GREEN, led);
-  turnOnIf(LED_BLUE, led);
+  turnOnIf(LED_LL, led);
+  turnOnIf(LED_L, led);
+  turnOnIf(LED_C, led);
+  turnOnIf(LED_R, led);
+  turnOnIf(LED_RR, led);
 }
 
 void sayHi() {
-  blink(LED_BLUE);
+  blink(LED_LL);
+  blink(LED_L);
+  blink(LED_C);
+  blink(LED_R);
+  blink(LED_RR);
   delay(userTime);
+}
+
+void readDiff() {
+  left = map(analogRead(A5), 0, 1023, 0, 1000);
+  right = map(analogRead(A4), 0, 1023, 0, 1000);
+  diff = left - right - calibration;
+}
+
+void calibrate() {
+  int sum = 0;
+  for (byte i = 0; i < calibrationTimes; i++) {
+    readDiff();
+    sum += diff;
+    blink(LED_C);
+    delay(25);
+  }
+  calibration = sum / calibrationTimes;
 }
 
 void setup() {
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_YELLOW, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
-  pinMode(SWITCH, INPUT_PULLUP);
+  Serial.begin(9600);
+  pinMode(LED_LL, OUTPUT);
+  pinMode(LED_L, OUTPUT);
+  pinMode(LED_C, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_RR, OUTPUT);
 
   turnOnOnly(0); // turn off all
+  sayHi();
+  calibrate();
+  sayHi();
 }
 
-void waitForSwitch() {
-  delay(userTime);
-  while(digitalRead(SWITCH) == HIGH) {}
-}
 
 void loop() {
-  turnOnOnly(LED_RED);
-  sayHi();
-  waitForSwitch();
-  turnOnOnly(LED_YELLOW);
-  delay(userTime * 3);
-  turnOnOnly(LED_GREEN);
-  delay(userTime * 8);
-  turnOnOnly(LED_YELLOW);
-  delay(userTime * 3);
+  readDiff();
+  Serial.println(diff);
+
+  if (diff >= 500) {
+    turnOnOnly(LED_LL);
+  } else if (diff >= 100) {
+    turnOnOnly(LED_L);
+  } else if (diff > -100) {
+    turnOnOnly(LED_C);
+  } else if (diff > -500) {
+    turnOnOnly(LED_R);
+  } else if (diff <= -500) {
+    turnOnOnly(LED_RR);
+  }
+
+  delay(20);
 }
