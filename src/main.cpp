4ca9fcd49591b62709018370240e4ce7ceab3be0
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2020-03-23 17:27:50 +0100

    Add two line LCD display

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
 #include <DHT.h>
+#include <rgb_lcd.h>
 #include "seeed_w600.h"
 #include "config.h"
 
+// WiFi
 SoftwareSerial softSerial(3, 2);
 
 AtWifi wifi;
@@ -14,45 +16,91 @@ uint16_t LOCAL_PORT = 8080;
 String msg;
 int32_t socket = -1;
 
+// Sensors
 DHT sensor(4, DHT11);
+
+// State
+int hundredsOfMilis = 0;
+
+// Display
 const uint8_t ledStatus = 7;
+rgb_lcd lcd;
+
+void displayJust(const char* text) {
+  lcd.clear();
+  lcd.print(text);
+}
+
+void displayError(const char* message) {
+  debug.println(message);
+  displayJust(message);
+  lcd.autoscroll();
+}
+
+void displayNetStatus(bool status) {
+  lcd.setCursor(14, 1);
+  lcd.print(status ? "+" : "-");
+}
+
+void displayReadingsPlaceholder() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp. ???`C  net");
+  lcd.setCursor(0, 1);
+  lcd.print("Wilg. ???%");
+}
+
+void displayReadings(int temp, int hum) {
+  lcd.setCursor(6, 0);
+  lcd.print("   ");
+  lcd.setCursor(6, 0);
+  lcd.print((int)temp);
+
+  lcd.setCursor(6, 1);
+  lcd.print("   ");
+  lcd.setCursor(6, 1);
+  lcd.print((int)hum);
+}
 
 void setup() {
+  debug.begin(9600);
+
   sensor.begin();
   pinMode(ledStatus, OUTPUT);
-  debug.begin(9600);
-  debug.println("Zaczynamy");
+  lcd.begin(16, 2);
+  lcd.setRGB(255, 0, 0);
+  displayJust("Connecting...");
 
   wifi.begin(softSerial, 9600);
   wifi.wifiReset();
   delay(1000);
   if(!wifi.wifiSetMode(STA)) {
-    debug.println("Set config mode failed!");
+    displayError("Set config mode failed!");
     digitalWrite(7, HIGH);
     return;
   }
   if (!wifi.wifiStaSetTargetApSsid(networkName)) {
-    debug.println("Set target AP ssid failed!!");
+    displayError("Set target AP ssid failed!!");
     digitalWrite(7, HIGH);
     return;
   }
   delay(100);
   if (!wifi.wifiStaSetTargetApPswd(neworkPassword)) {
-    debug.println("Set target AP password failed!!");
+    displayError("Set target AP password failed!!");
     digitalWrite(7, HIGH);
     return;
   }
   delay(100);
 
   if (!wifi.joinNetwork()) {
-    debug.println("Join to AP network failed!!");
+    displayError("Join to AP network failed!!");
     digitalWrite(7, HIGH);
     return;
   }
   delay(1500);
   if (!wifi.wifiCreateSocket(msg, TCP, Client, TARGET_IP, TARGET_PORT,
                              LOCAL_PORT)) {
-    debug.println("Connect to remote server failed!!");
+    displayError("Connect to remote server failed!!");
     digitalWrite(7, HIGH);
     return;
   }
@@ -61,27 +109,35 @@ void setup() {
   debug.print("socket = ");
   debug.println(socket);
   debug.println("Wifi connected, TCP connection established");
+  displayJust("Connected");
+  delay(2000);
+
+  displayReadingsPlaceholder();
 }
 
-void loop() {
+void doTheReadings() {
   float hum  = sensor.readHumidity();
   float temp = sensor.readTemperature();
 
   if (isnan(hum) || isnan(temp) || (hum == 0.0 && temp == 0.0)) {
-    digitalWrite(7, HIGH);
+    displayReadingsPlaceholder();
   } else {
-    digitalWrite(7, LOW);
     String data = "0,";
     data += temp;
     data += ",";
     data += hum;
     data += "\n";
-    wifi.wifiSocketSend(msg, socket, data);
-    // Serial.print((int)hum);
-    // Serial.print(",");
-    // Serial.print((int)temp);
-    // Serial.println();
+    bool netStatus = wifi.wifiSocketSend(msg, socket, data);
+    displayReadings(temp, hum);
+    displayNetStatus(netStatus);
   }
+}
 
+void loop() {
+  digitalWrite(7, HIGH);
+  delay(100);
+  digitalWrite(7, LOW);
+
+  doTheReadings();
   delay(30000);
 }
