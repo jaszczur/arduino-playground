80df985a50a52f1056552615d8c2810b3018331f
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2020-03-27 19:14:27 +0100

    IR receiver to debug TV remote signals

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 #include <Grove_Motor_Driver_TB6612FNG.h>
+#include <IRSendRev.h>
 
 MotorDriver motor;
 
-void setup() {
+
+void setupMotor() {
   Wire.begin();
   Serial.begin(9600);
   motor.init();
@@ -13,7 +15,7 @@ void setup() {
 }
 
 
-void loop() {
+void loopMotor() {
   Serial.println("run at speed=200");
   motor.dcMotorRun(MOTOR_CHA, 200);
   motor.dcMotorRun(MOTOR_CHB, 200);
@@ -37,4 +39,56 @@ void loop() {
   motor.dcMotorStop(MOTOR_CHA);
   motor.dcMotorStop(MOTOR_CHB);
   delay(1000);
+}
+
+#define BIT_LEN       0
+#define BIT_START_H   1
+#define BIT_START_L   2
+#define BIT_DATA_H    3
+#define BIT_DATA_L    4
+#define BIT_DATA_LEN  5
+#define BIT_DATA      6
+
+unsigned char data[24];
+
+void setupReceiver() {
+  Serial.begin(115200);
+  IR.Init(2);
+}
+
+
+void loopReceiver() {
+  if(IR.IsDta()) {
+    IR.Recv(data);
+
+    Serial.print("{:len ");
+    Serial.println(data[BIT_LEN]);
+
+    Serial.print(" :start-h ");
+    Serial.println(data[BIT_START_H]);
+
+    Serial.print(" :start-l ");
+    Serial.println(data[BIT_START_L]);
+
+    Serial.print(" :data-h ");
+    Serial.println(data[BIT_DATA_H]);
+
+    Serial.print(" :data-l ");
+    Serial.println(data[BIT_DATA_L]);
+
+    Serial.print(" :data [");
+    for (int i = BIT_DATA; i < BIT_DATA + data[BIT_DATA_LEN]; i++) {
+      Serial.print(data[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println("]\n}\n------------------------------------");
+  }
+}
+
+void setup() {
+  setupReceiver();
+}
+
+void loop() {
+  loopReceiver();
 }
