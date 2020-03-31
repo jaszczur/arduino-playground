7e1e17fd12d9e9f426c94aac1657e37a9d9f6fbd
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2020-03-31 17:08:39 +0200

    Moved to Automaton
    For FSM, Events and overal code structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,18 @@
 #include <Arduino.h>
-#include <limits.h>
+#include <Automaton.h>
 
 #define PIN_BUTTON 3
 #define PIN_LED    10
 
-class ButtonFSM {};
-
-class LedFSM {};
-
-struct State {
-  bool ledBlinking = false;
-  bool ledOn = false;
-  unsigned long nextBlink = ULONG_MAX;
-
-  unsigned long buttonPressedTime = 0;
-  unsigned long buttonUnpress = 0;
-
-  unsigned long now = 0;
-};
-
-State state;
+Atm_led led;
+Atm_button button;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(PIN_LED, OUTPUT);
-  pinMode(PIN_BUTTON, INPUT_PULLUP);
+  led.begin(PIN_LED).blink(200, 200);
+  button.begin(PIN_BUTTON).onPress(led, led.EVT_TOGGLE_BLINK);
 }
 
 void loop() {
-  // read state from sensors
-  bool buttonPressed = !digitalRead(PIN_BUTTON);
-  state.now = millis();
-
-  // update current state
-  if (state.buttonPressedTime == 0) {
-    if (buttonPressed) {
-      Serial.println("arming");
-      state.buttonPressedTime = state.now + 40;
-    }
-  } else if (state.now >= state.buttonPressedTime) {
-    if (buttonPressed) {
-      Serial.println("button is still pressed. it's for real");
-      state.ledBlinking = !state.ledBlinking;
-
-      if (state.ledBlinking) {
-        state.nextBlink = state.now;
-      } else {
-        state.nextBlink = ULONG_MAX;
-        state.ledOn = false;
-      }
-    } else {
-      Serial.println("nope");
-    }
-    state.buttonPressedTime = 0;
-  } else {
-    Serial.println("hold");
-  }
-
-  if (state.now >= state.nextBlink && state.ledBlinking) {
-    state.ledOn = !state.ledOn;
-    state.nextBlink = state.now + 500;
-  }
-
-  // set actuators
-  digitalWrite(PIN_LED, state.ledOn);
-
-  // reset state
-  //memcpy(prevState, state, sizeof(State));
+  automaton.run();
 }
