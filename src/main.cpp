a7c889c0fdc5a875a1efd23171fafe34af6bd2f6
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2020-04-01 17:01:17 +0200

    second led

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,30 @@
 #include <Arduino.h>
 #include <Automaton.h>
 
-#define PIN_BUTTON 3
-#define PIN_LED    10
+#define PIN_BUTTON       3
+#define PIN_LED_YELLOW  10
+#define PIN_LED_GREEN   11
 
-Atm_led led;
+Atm_led ledYellow;
+Atm_led ledGreen;
 Atm_button button;
+Atm_fan buttonPressed;
 
 void setup() {
   Serial.begin(9600);
-  led.begin(PIN_LED).blink(200, 200);
-  button.begin(PIN_BUTTON).onPress(led, led.EVT_TOGGLE_BLINK);
+  ledYellow.begin(PIN_LED_YELLOW).blink(200, 200);
+  ledGreen.begin(PIN_LED_GREEN);
+  buttonPressed.begin();
+
+  button.begin(PIN_BUTTON)
+    .onPress(buttonPressed, buttonPressed.EVT_INPUT)
+    .onRelease(ledGreen, ledYellow.EVT_OFF);
+
+  buttonPressed
+    .onInput(ledYellow, ledYellow.EVT_TOGGLE_BLINK)
+    .onInput(ledGreen, ledYellow.EVT_ON);
+
+  button.trace(Serial);
 }
 
 void loop() {
